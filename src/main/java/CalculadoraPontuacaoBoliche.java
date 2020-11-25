3d2f6d0846b4a24ed810af5a94bc6ebdc422de00
diogenes.rafael <diogenes.rafael@crptecnologia.com.br>
2020-11-25 00:18:47 -0300

    Criando Testes.

--- src/main/java/CalculadoraPontuacaoBoliche.java
+++ src/main/java/CalculadoraPontuacaoBoliche.java
@@ -1,7 +1,58 @@
-import java.util.Arrays;
-
 public class CalculadoraPontuacaoBoliche {
+
+    private int[] jogadas;
+    private int qtdStrike=0;
+
     int pontuacaoDoJogo(int[] jogadas) {
-        return Arrays.stream(jogadas).sum();
+        this.jogadas = jogadas;
+        var resultado = 0;
+        for (var indice = 0; indice < jogadas.length; indice++) {
+            resultado += jogadas[indice];
+            if(ehStrike(indice)){
+                resultado += somaBonusStrike(indice);
+            } else if (ehSpare(indice)) {
+                resultado += somaBonusSpare(indice);
+            }
+        }
+        return resultado;
     }
+
+    private int somaBonusSpare(int indice) {
+        return jogadas[indice+2];
+    }
+
+    private int somaBonusStrike(int indice) {
+        return jogadas[indice+1] + jogadas[indice+2];
+    }
+
+    private boolean ehStrike(int indice) {
+        if (jogadas[indice] == 10 && ehPrimeiraJogada(indice+qtdStrike)){
+            qtdStrike ++;
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    private boolean ehSpare(int indice) {
+        return ehPrimeiraJogada(indice) && ehUltimoLance(indice, jogadas.length - 2) && ehSoma10(indice);
+    }
+
+    private boolean ehSoma10(int indice) {
+        return this.jogadas[indice] + this.jogadas[indice + 1] == 10;
+    }
+
+    private boolean ehUltimoLance(int indice, int tamanhoArray) {
+        return indice < tamanhoArray;
+    }
+
+    private boolean ehPrimeiraJogada(int indice) {
+        return ehPar(indice);
+    }
+
+    private boolean ehPar(int indice) {
+        return indice % 2 == 0;
+    }
+
 }
--- src/test/java/CalculadoraPontuacaoBolicheTest.java
+++ src/test/java/CalculadoraPontuacaoBolicheTest.java
@@ -1,11 +1,11 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CalculadoraPontuacaoBolicheTest {
 
     @Test
-    void deve_retornar_zero_quando_errar_todas_as_jogadas() {
+    public void deve_retornar_zero_quando_errar_todas_as_jogadas() {
         //arrange
         var jogadas = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
@@ -13,8 +13,80 @@ public class CalculadoraPontuacaoBolicheTest {
         var result = new CalculadoraPontuacaoBoliche().pontuacaoDoJogo(jogadas);
 
         //assert
-        assertEquals(result, 0);
+        assertEquals(0, result);
     }
+
+
+
+    @Test
+    public void deve_retornar_soma_de_jogo_com_spare() {
+        //arrange
+        var jogadas = new int[]{0, 0,
+                                0, 0,
+                                0, 0,
+                                0, 0,
+                                0, 0,
+                                0, 0,
+                                0, 0,
+                                2, 8,
+                                2, 3,
+                                0,0};
+
+        //act
+        var result = new CalculadoraPontuacaoBoliche().pontuacaoDoJogo(jogadas);
+
+        //assert
+        assertEquals(17, result);
+    }
+
+    @Test
+    public void deve_retornar_soma_de_jogo_com_strike() {
+        //arrange
+        var jogadas = new int[]{0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                0, 0,
+                10,
+                2, 3,
+                0, 0};
+
+        //act
+        var result = new CalculadoraPontuacaoBoliche().pontuacaoDoJogo(jogadas);
+
+        //assert
+        assertEquals(20, result);
+    }
+
+
+    @Test
+    public void deve_retornar_soma_de_jogo_com_strike_e_spare() {
+        //arrange
+        var jogadas = new int[]{
+                1, 4,
+                4, 5,
+                6, 4,
+                5, 5,
+                10,
+                0, 1,
+                7, 3,
+                6, 4,
+                10,
+                2, 8,
+                6};
+
+        //act
+        var result = new CalculadoraPontuacaoBoliche().pontuacaoDoJogo(jogadas);
+
+        //assert
+        assertEquals(133, result);
+    }
+
+
+
+
 
 }
 
